cfc64d87259937e20b79fc42f2331abf2d859768
Laurent Laborde <kerdezixe@gmail.com>
2011-01-02 19:15:52 +0100

    cleaning useless code

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 
 void main(int argc, char **argv) {
 
-	FILE *instream;
-	FILE *outstream;
-
 	char buffer[MD5_BLOCK_LENGTH];
 
 	size_t buffer_size = sizeof(char) * (MD5_BLOCK_LENGTH);
@@ -20,23 +17,14 @@ void main(int argc, char **argv) {
 	//initialisation
 	md5_reset(&md5);
 
-	instream = fopen("/dev/stdin","r");
-	if(instream == NULL) { printf("can't open /dev/stdin"); exit(1); }
-
-	outstream = fopen("/dev/stdout","w");
-	if(outstream == NULL) { printf("can't open /dev/stdout"); exit(1); }
-
-
 	while(1) {
-		bytes_read = fread(&buffer,sizeof(char), 64, instream); 
+		bytes_read = fread(&buffer,sizeof(char), 64, stdin); 
 		md5_update(&md5,&buffer, bytes_read);
-		fwrite(&buffer, sizeof(char), bytes_read / sizeof(char), outstream);
+		fwrite(&buffer, sizeof(char), bytes_read / sizeof(char), stdout);
 		if(bytes_read == 0) { break; }
 	}
 	md5_final(digest,&md5);
 	hash2hex(digest,printable_digest);
 	fprintf(stderr,"%s",printable_digest);
 
-	fclose(instream);
-	fclose(outstream);
 }

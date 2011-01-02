f0c482b906059267cc1d74afad764ac19d1d63a8
Laurent Laborde <kerdezixe@gmail.com>
2011-01-02 23:05:18 +0100

    code cleaning

--- main.c
+++ main.c
@@ -6,23 +6,23 @@ void main(int argc, char **argv) {
 
 	char buffer[MD5_BLOCK_LENGTH];
 
-	size_t buffer_size = sizeof(char) * (MD5_BLOCK_LENGTH);
+	size_t buffer_size = MD5_BLOCK_LENGTH;
 	size_t bytes_read = 0;
-
-	uint64_t total_byte_read = 0;
-	struct md5_ctx md5;
 	uint8_t digest[MD5_DIGEST_LENGTH];
+
 	char printable_digest[MD5_DIGEST_LENGTH];
 
-	//initialisation
+	struct md5_ctx md5;
+
 	md5_reset(&md5);
 
 	while(1) {
-		bytes_read = fread(&buffer,sizeof(char), 64, stdin); 
-		md5_update(&md5,&buffer, bytes_read);
-		fwrite(&buffer, sizeof(char), bytes_read / sizeof(char), stdout);
+		bytes_read = fread(&buffer,1 , 64, stdin); 
 		if(bytes_read == 0) { break; }
+		md5_update(&md5,&buffer, bytes_read);
+		fwrite(&buffer, sizeof(char), bytes_read, stdout);
 	}
+
 	md5_final(digest,&md5);
 	hash2hex(digest,printable_digest);
 	fprintf(stderr,"%s",printable_digest);
